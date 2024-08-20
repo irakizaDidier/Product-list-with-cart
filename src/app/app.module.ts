a2e6025c77e5c3a860f52de9ac4993cc13460a27
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-08-20 17:39:14 +0200

    creating cart component for displaying cart products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,7 @@ import { ModalContentComponent } from './sections/modal-content/modal-content.co
     DessertsComponent,
     ModalContentComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ImageSizePipe],
+  imports: [BrowserModule, AppRoutingModule, ImageSizePipe, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/sections/cart/cart.component.ts
+++ src/app/sections/cart/cart.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/products.model';
+import { ModalContentComponent } from '../modal-content/modal-content.component';
+import { ToastrService } from 'ngx-toastr';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  private service = inject(ProductsService);
+  private modalService = inject(NgbModal);
+  private toastr = inject(ToastrService);
 
+  products = this.service.products;
+
+  cartProducts = computed(() =>
+    this.products().filter((product) => product.quantity)
+  );
+
+  total = computed(() =>
+    this.products().reduce(
+      (accumulator, product) => accumulator + product.quantity,
+      0
+    )
+  );
+
+  totalPrice = computed(() =>
+    this.products().reduce(
+      (accumulator, product) => accumulator + product.quantity * product.price,
+      0
+    )
+  );
+
+  removeProduct(product: Product): void {
+    this.service.removeProduct(product);
+  }
+
+  open() {
+    const modalRef: NgbModalRef = this.modalService.open(
+      ModalContentComponent,
+      {
+        centered: true,
+        size: 'lg',
+      }
+    );
+    modalRef.componentInstance.products = this.products;
+
+    modalRef.dismissed.subscribe((reason: any) => {
+      if (reason === 2) {
+        this.service.startNewOrder();
+        this.toastr.success('The order has been confirmed!');
+      }
+    });
+  }
 }
